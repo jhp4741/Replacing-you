058406110b2e655d8aea2c0b71cae0ad65f08eef
202133408ksy <120313280+202133408ksy@users.noreply.github.com>
2022-12-12 18:39:44 +0900

    Update load_image.py

---
+++ load_image.py
@@ -0,0 +1,8 @@
+import cv2
+img_gray = cv2.imread('C:/test.jpg',0) #gray 사진 읽기
+img_color = cv2.imread('C:/test.jpg',1) #color 사진 읽기
+
+cv2.imshow('gray', img_gray)
+cv2.imshow('color', img_color)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
