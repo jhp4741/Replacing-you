2ac1b6e8621634c4ed88cfec4c02cc11415addd1
202133408ksy <120313280+202133408ksy@users.noreply.github.com>
2022-12-12 18:39:59 +0900

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -1,6 +1,6 @@
 import cv2
-img_gray = cv2.imread('C:/test.jpg',0) #gray 사진 읽기
-img_color = cv2.imread('C:/test.jpg',1) #color 사진 읽기
+img_gray = cv2.imread('C:/test.jpg',0) # gray 사진 읽기
+img_color = cv2.imread('C:/test.jpg',1) # color 사진 읽기
 
 cv2.imshow('gray', img_gray)
 cv2.imshow('color', img_color)
