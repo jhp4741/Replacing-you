cf48dff8f612c9e3758c356b3e18a4bab1899388
Kyungdeok Roh <shrudejr5207@gmail.com>
2022-12-13 04:34:48 +0900

    solve bilateral symmetry

--- connect_carmera.py
+++ connect_carmera.py
@@ -9,6 +9,7 @@ if not carmera.isOpened(): #웹캠이 감지되지 않으면
 else:
 	while carmera.isOpened():
 		status, frame = carmera.read() #웹캠에서 촬영되는 영상을 읽어냄
+		frame = cv2.flip(frame,1)# 좌우대칭을 마춰줌
 		if status:
 			cv2.imshow("test", frame) # 읽어낸 영상을 test라는 창에 출력
 		if cv2.waitKey(1) & 0xFF == ord('q'): #q를 입력할씨 출력 중단.
